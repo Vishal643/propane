88485074c4abae17ecaa264e96478de567473efb
Gayathry <gayathryjm@gmail.com>
2021-03-11 15:24:18 +0530

    Recent Changes

--- src/Components/BusinessBody/BusinessBody.jsx
+++ src/Components/BusinessBody/BusinessBody.jsx
@@ -56,8 +56,74 @@ const BusinessBody = () => {
 					</div>
 				</div>
 			</div>
-			<IndiaBusiness />
-			<InternationalBusiness />
+			
+			<div>
+				<div className={styles.business_head}>
+					<p>
+						INDIA BUSINESS <strong> NEWS </strong>
+					</p>
+					<div></div>
+				</div>
+
+				<div className={styles.business_news_wrapper}>
+					<div className={styles.business_news_main}>
+						<img
+							src='https://img.etimg.com/thumb/msid-76005001,width-300,imgsize-115155,,resizemode-4,quality-100/.jpg'
+							alt='pic'
+							width='101%'
+							height='250px'
+						/>
+						<p>Travelers gaining confidence, time to plan for restart: IATA</p>
+					</div>
+
+					<div className={styles.business_newshead_wrapper}>
+						{businessIndia?.map((item, i) => (
+							<div className={styles.business_news_head} key={i}>
+								<div>
+									<Link to={`/business/india/${item.id}`}>{item.headline}</Link>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+
+			
+			<div>
+				<div className={styles.business_head}>
+					<p>
+						INTERNATIONAL BUSINESS <strong> NEWS </strong>
+					</p>
+					<div style={{ width: '60%' }}></div>
+				</div>
+
+				<div className={styles.business_news_wrapper}>
+					<div className={styles.business_news_main}>
+						<img
+							src='https://vid.alarabiya.net/images/2020/09/30/9fd1390d-fabc-493f-a0eb-6fb51a05c828/9fd1390d-fabc-493f-a0eb-6fb51a05c828_16x9_600x338.jpg'
+							alt='pic'
+							width='100%'
+							height='230px'
+						/>
+						<p>
+							Joe Biden flashes warning to big tech as antitrust team takes
+							shape
+						</p>
+					</div>
+
+					<div className={styles.business_newshead_wrapper}>
+						{economyNews?.map((item, i) => (
+							<div className={styles.business_news_head} key={i}>
+								<div>
+									<Link to={`/business/international/${item.id}`}>
+										{item.headline}
+									</Link>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
 		</>
 	);
 };
